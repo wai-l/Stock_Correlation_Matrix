8187519d225e70195bde4b7db234c1d2d0425c31
to-waiting-room <to.waiting.room@gmail.com>
2025-03-02 13:04:43 +0000

    Tested script for updated YFinance package Changes to be committed: 	modified:   stock_api.py

--- stock_api.py
+++ stock_api.py
@@ -4,8 +4,8 @@ import streamlit as st
 
 # def main():
 #     tickers = ["AAPL", "MSFT", "NVDA"]
-#     start_date = "2022"
-#     end_date = "2024-01-01"
+#     start_date = "2024-01-01"
+#     end_date = "2025-01-01"
 
 #     print(ticker_closed_price(tickers, start_date, end_date))
 
